04ce6703088cd687cfad952aa81a4d71cb9249f4
Ed Ignot <edignot@gmail.com>
2020-02-04 21:29:15 -0700

    past log css box

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+// warning when accomplishment message is empty
 var inputAccomplish = document.querySelector('.input-accomplish');
 var warningMessage = document.querySelector('.warning-message');
 var startButton = document.querySelector('.start');
-var heading = document.querySelector('h1');
+
 startButton.addEventListener('click', function () {
   if (inputAccomplish.value === '') {
     warningMessage.style.display = 'block';
+    inputAccomplish.style.borderBottom = '2px solid #EFB7EC';
+  } else {
+    warningMessage.style.display = 'none';
+  }
+});
+
+
+
+// WARNING: not working!! min / sec input accepts only positive number
+var inputTime = document.querySelector('.input-min-sec');
+
+inputTime.addEventListener('keypress', function () {
+  if (inputType.value < 0) {
+    alert('enter positive value!');
   }
 });
